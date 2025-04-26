c52473a03b47e64e538f0e51f6f84639f6d70d35
David Santillan <david7b1930@gmail.com>
2025-04-26 13:06:57 -0500

    First release

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+
+import './App.css'
+
+function App() {
+
+    return (
+        <>
+            <article>
+                <p>Clase DWFS</p>
+            </article>
+
+            <section>
+                <p>23 abril</p>
+            </section>
+            <footer>
+                <p>2025</p>
+            </footer>
+        </>
+    )
+}
+
+export default App
